ce759393c8620338f913a2028b8a64f9b8505451
KamilGmitrzuk <gmitrzukkamil@gmail.com>
2023-04-15 21:15:27 +0200

    update

--- klient/app.js
+++ klient/app.js
@@ -1,8 +1,19 @@
-async function logowanie(){
-    const inputl = document.getElementById("inputl").value
-    const inputh = document.getElementById("inputh").value
+async function logowanie() {
+  const inputl = document.getElementById("inputl").value;
+  const inputh = document.getElementById("inputh").value;
 
-    const data = await fetch(`${baseurl}/checkpassword/${inputl}/${inputh}`)
-    var json = data.json()
-    console.log(json)
+  const data = await fetch(`${baseurl}/checkpassword/${inputl}/${inputh}`).then(
+    (response) => response.text()
+  );
+  console.log(data);
+  if (data == "access") {
+    localStorage.setItem("login", "zalogowano");
+  } else {
+    localStorage.setItem("login", "nie zalogowano");
+  }
+}
+function checklogin() {
+  if (localStorage.getItem("login") != "zalogowano") {
+    window.location.href = "index.html";
+  }
 }
